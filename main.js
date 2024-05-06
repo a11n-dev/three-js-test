3da288a0c6af6e8a2e6ea956c0ca11d22246fa9b
Andrew E <im.a11n.dev@gmail.com>
2024-05-06 14:34:17 +0200

    Update camera settings for better model visualization

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
 
 // Create camera
-const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 2000);
 
 // Create camera
 // const aspect = window.innerWidth / window.innerHeight;
